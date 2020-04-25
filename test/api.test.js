38e35d395292d79497c713c86c4eef6d9c28ed7b
lucas-oliveira17 <lucas_oliveira_viana@hotmail.com>
2020-04-25 17:40:27 -0300

    inserindo teste de integração

--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,10 @@
-describe('user api tests', () => {
+const request = require('supertest')
+const app = require('../src/server')
 
+describe('user api tests', () => {
     test('post user', async () => {
-        
+        const response = await request(app).get('/users').send();
+
+        expect(response.status).toBe(200);
     })
 });
